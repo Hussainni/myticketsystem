1a6cb20592fce810a35b0a12ef3e66919857b3d1
Hussain Ahmad <hussainahmad.dev.17@gmail.com>
2025-09-10 14:52:15 +0500

    edit urls

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const app = express();
 
 // app.use(cors());
 app.use(cors({
-  origin: 'https://myticketsystems.vercel.app',
+  origin: ['https://myticketsystems.vercel.app', 'http://localhost:5173'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
